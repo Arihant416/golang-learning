bcfc817f4e662c7c93913e3c84bcd5bdc6841d81
Arihant416 <arihantjain416@gmail.com>
2025-08-02 21:42:06 +0530

    Structs in Go: Basics

---
+++ phase-2/structs-learning/app.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthDate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var person user = user{
+		firstName,
+		lastName,
+		birthDate,
+		time.Now(),
+	}
+
+	outputUserDetails(&person)
+
+	// fmt.Println(firstName, lastName, birthDate)
+}
+
+func outputUserDetails(person *user) {
+	fmt.Println("FirstName", person.firstName)
+	fmt.Println("LastName", person.lastName)
+	fmt.Println("BirthDay", person.birthDate)
+	fmt.Println("CreatedAt", person.createdAt)
+
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
