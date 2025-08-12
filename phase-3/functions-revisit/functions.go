8c0627980b076554d55ff21f49d15744a87985dd
Arihant Jain <arihantjain416@gmail.com>
2025-08-12 03:31:36 +0000

    Functions as values & functions types

---
+++ phase-3/functions-revisit/functions.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+
+type transformFn func(int) int
+
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	doubledManual := doubleNumbers(&numbers)
+	doubled := transform(&numbers, double)
+	tripled := transform(&numbers, triple)
+
+	fmt.Println(doubledManual)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+}
+
+func doubleNumbers(numbers *[]int) []int {
+	doubled := []int{}
+	for _, num := range *numbers {
+		doubled = append(doubled, double(num))
+	}
+	return doubled
+}
+
+func transform(numbers *[]int, transformer transformFn) []int {
+	transformed := []int{}
+	for _, num := range *numbers {
+		transformed = append(transformed, transformer(num))
+	}
+	return transformed
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
